737da9c5b507bd53c9eeb21aa22faeb4bb17fce5
pjay2253 <101237111+pjay2253@users.noreply.github.com>
2022-11-09 11:50:04 -0500

    change syntax code

--- JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
+++ JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
@@ -6,12 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
 
-		string name = "paul";
+        String name = "paul";
 
-		system.out.println(name);
-	}
-
+        System.out.println(name);
+    }
+	
 }
